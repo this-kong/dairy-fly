fb4eaced645d62641d18c19061b3a99ad4959274
sleepingVENNIE <2248893785@qq.com>
2026-05-26 10:42:37 +0800

    fix getClient import

--- app/api/book/route.ts
+++ app/api/book/route.ts
@@ -1,4 +1,5 @@
-import { connectDB, getClient } from '@/lib/mongodb';
+// import { connectDB, getClient } from '@/lib/mongodb';
+import { connectDB } from '@/lib/mongodb';
 import { NextRequest, NextResponse } from 'next/server';
 import { ObjectId } from 'mongodb';
 import crypto from 'crypto';
